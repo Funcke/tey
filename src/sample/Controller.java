7eee87c6a83d960c766836ac5f9cd4bd103c9122
Jonas Funcke <40146720150244@litec.ac.at>
2017-09-26 19:35:55 +0200

    Added core window where application takes place

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,9 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -14,6 +17,25 @@ public class Controller {
     public void LogIn() {
         System.out.println(un.getText());
         System.out.println(pw.getText());
+        this.Open();
+    }
+
+    private void Open() {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Core.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Main");
+            stage.setScene(new Scene(root, 450, 450));
+            stage.show();
+
+            Stage this_form = (Stage) discard.getScene().getWindow();
+            this_form.close();
+        }
+        catch(Exception err)
+        {
+            ;
+        }
     }
 
     public void Discard() {
