1e364656c0eeaee984c77d411bf3ae5796698e3f
Jonas Funcke <40146720150244@litec.ac.at>
2017-09-27 18:16:23 +0200

    Added the login mechanism

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,19 +5,45 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.beans.EventHandler;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class Controller {
     @FXML TextField un;
     @FXML TextField pw;
     @FXML Button discard;
+    @FXML Label warning;
     public void LogIn() {
         System.out.println(un.getText());
         System.out.println(pw.getText());
-        this.Open();
+        if(this.Control(un.getText(), pw.getText()))
+            this.Open();
+        else
+            warning.setText("Wrong Log in date");
+    }
+
+    private boolean Control(String uname, String pword) {
+        try {
+            BufferedReader source = new BufferedReader(new FileReader("user.csv"));
+            String line = source.readLine();
+
+            while (line != null) {
+                if (line.equals(uname + ";" + pword))
+                    return true;
+                line = source.readLine();
+            }
+        }
+        catch(Exception err){
+            System.out.println(err.getMessage());
+        }
+        return false;
     }
 
     private void Open() {
@@ -37,6 +63,36 @@ public class Controller {
             ;
         }
     }
+
+    public void Sign_up () {
+        String uName = un.getText();
+        String pWord = pw.getText();
+        String line;
+        try {
+            BufferedReader data = new BufferedReader(new FileReader("user.csv"));
+
+            line = data.readLine();
+            while (line != null) {
+                if (line.equals(uName + ';' + pWord)){
+                    warning.setText("Attention! This user already exists!");
+                    break;
+                }
+                line = data.readLine();
+            }
+
+            data.close();
+
+            BufferedWriter entry = new BufferedWriter(new FileWriter("user.csv", true));
+            entry.write(uName + ";" + pWord + "\n");
+            entry.close();
+        }
+        catch(Exception err)
+        {
+            System.out.println(err.getMessage());
+        }
+
+    }
+
 
     public void Discard() {
         Stage stage = (Stage) discard.getScene().getWindow();
