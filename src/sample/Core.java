8621d038f14fc692524856aa7d0f5fb1a5bca72f
Jonas Funcke <40146720150244@litec.ac.at>
2017-09-27 12:58:22 +0200

    Added saving functionality

--- src/sample/Core.java
+++ src/sample/Core.java
@@ -3,13 +3,26 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 
+import java.io.PrintWriter;
+
 public class Core {
     @FXML TextArea Input;
     public void Save() {
+        String content = Input.getText();
+        int i = content.indexOf(' ');
+        String name = content.substring(0, i) + ".txt";
 
+        try {
+            PrintWriter target = new PrintWriter(name, "utf-8");
+            target.write(content);
+            target.close();
+        }
+        catch(Exception err) {
+            System.out.println(err.getMessage());
+        }
     }
 
     public void Discard() {
-
+        Input.setText("");
     }
 }
