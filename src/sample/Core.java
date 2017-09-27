05ebc525d030af37122e081fa715a9a8ba8ee498
Jonas Funcke <40146720150244@litec.ac.at>
2017-09-27 09:21:55 +0200

    Added text Input

--- src/sample/Core.java
+++ src/sample/Core.java
@@ -1,5 +1,15 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class Core {
-    
+    @FXML TextArea Input;
+    public void Save() {
+
+    }
+
+    public void Discard() {
+
+    }
 }
